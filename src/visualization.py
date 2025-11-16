3466fb3c3272ab977389d5988783c652b5b84b25
CherrIt <caotienthanh1103@nthanh1103gmail.com>
2025-11-16 11:18:41 +0700

    update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -2,16 +2,28 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def visua_hist(data, title):
+def visua_hist(data, title, xlabel, ylabel):
     # Vẽ histogram
     sns.histplot(data, bins=30, kde=True)
     plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.show()
 
 
-# plt.figure(figsize=(12,6))
-# sns.heatmap(missing_mask, cbar=False)
-# plt.title("Heatmap Missing Values")
-# plt.xlabel("Columns")
-# plt.ylabel("Rows")
-# plt.show()
+def visua_boxplot(data, title, xlabel, ylabel):
+    # Vẽ boxplot
+    plt.boxplot(data, patch_artist=True)
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+
+def visua_scatter(data_x, data_y, title, xlabel, ylabel):
+    # Vẽ scatter plot
+    plt.scatter(data_x, data_y, alpha=0.3)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)    
+    plt.show()
